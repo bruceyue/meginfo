8c02fbeaa48a517bd012a69fd22a89686b0d5676
Bruce Yue <bruce.yue@meginfo.com>
2012-03-28 23:16:40 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SfdcRailsDemo::Application.config.secret_token = 'e4d44a6a86e19822683a1bbdd5dc5e28899da7c87504b0906de5ef9fe128340c97d8798c8c1982ecb63d4f395e002e1bfa33b99ffceab7830f874f0b45ab8c07'
